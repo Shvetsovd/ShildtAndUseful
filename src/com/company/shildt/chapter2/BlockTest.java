1e80bb134a9d737233555c9900ad7bbf4ebeef17
Shvetsovd <shvetsovd@gmail.com>
2017-02-10 01:15:56 +0300

    Revert: Create test

--- src/com/company/shildt/chapter2/BlockTest.java
+++ src/com/company/shildt/chapter2/BlockTest.java
@@ -8,7 +8,7 @@ public class BlockTest {
     public static void main(String[] args) {
         int x, y;
 
-        y = 30;
+        y = 20;
 
         for (x = 0; x < 10; x++){
             System.out.println("Значение x: " + x);
