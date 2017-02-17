c1cd5342e0b4b7d6ed0fae18470345c9f18e4830
Shvetsovd <shvetsovd@gmail.com>
2017-02-18 00:49:41 +0300

    commit#17022017

--- src/com/company/shildt/chapter11/DemoJoin.java
+++ src/com/company/shildt/chapter11/DemoJoin.java
@@ -3,6 +3,53 @@ package company.shildt.chapter11;
 /**
  * Created by dmitry on 17.02.17.
  */
-public class DemoJoin {
+class NewThread4 implements Runnable {
+    String name;
+    Thread t;
 
+    public NewThread4(String name) {
+        this.name = name;
+        t= new Thread(this, name);
+        System.out.println("Новый поток: " + t);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(name + ": " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(name + " прерван");
+            }
+        }
+        System.out.println(name + " завершен");
+    }
+}
+
+public class DemoJoin {
+    public static void main(String[] args) {
+        NewThread4 ob1 = new NewThread4("Один");
+        NewThread4 ob2 = new NewThread4("Два");
+        NewThread4 ob3 = new NewThread4("Три");
+
+        System.out.println("Поток Один запущен: " + ob1.t.isAlive());
+        System.out.println("Поток Два запущен: " + ob2.t.isAlive());
+        System.out.println("Поток Три запущен: " + ob3.t.isAlive());
+
+        try {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван");
+        }
+
+        System.out.println("Поток Один запущен: " + ob1.t.isAlive());
+        System.out.println("Поток Два запущен: " + ob2.t.isAlive());
+        System.out.println("Поток Три запущен: " + ob3.t.isAlive());
+
+        System.out.println("Главный поток завершен");
+    }
 }
