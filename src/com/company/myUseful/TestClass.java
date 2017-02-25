fbb233ede2ff0eef28b33f61ee30bd5e50ee3556
Shvetsovd <shvetsovd@gmail.com>
2017-02-25 23:52:38 +0300

    TestCommit

---
+++ src/com/company/myUseful/TestClass.java
@@ -0,0 +1,23 @@
+package company.myUseful;
+
+/**
+ * Created by user on 25.02.2017.
+ */
+public class TestClass {
+    private long l;
+
+    public long getL() {
+        return l;
+    }
+
+    public void setL(long l) {
+        this.l = l;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClass{" +
+                "l=" + l +
+                '}';
+    }
+}
