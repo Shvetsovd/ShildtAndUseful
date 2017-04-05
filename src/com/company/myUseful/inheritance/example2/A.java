fa18e10f98b381428af51ad4bbebf5636b38c3e8
Shvetsovd <shvetsovd@gmail.com>
2017-04-05 19:27:32 +0400

    05042017

---
+++ src/com/company/myUseful/inheritance/example2/A.java
@@ -0,0 +1,53 @@
+package company.myUseful.inheritance.example2;
+
+interface IBuffer{
+    int DEFAULT_SIZE = 256;
+    int MAX_SIZE = (1<<31 - 1);
+
+    default byte[] getBuffer(){
+        return new byte[DEFAULT_SIZE];
+    }
+    static void fill(byte[] buf, byte value){
+        for (int i = 0; i < buf.length; i++)
+            buf[i] = value;
+    }
+    void copyTo(byte[] newBuf);
+    void clear();
+}
+
+abstract class A {
+    static {
+        System.out.println("Static block A");
+    }
+    {
+        System.out.println("Non-static block A");
+    }
+    public A(int a) {
+        super();
+        System.out.println("A(int a)");
+    }
+}
+
+class B extends A {
+
+    static {
+        System.out.println("Static block B");
+    }
+    {
+        System.out.println("Non-static block B");
+    }
+    B(){
+        super(1);
+        System.out.println("B()");
+    }
+}
+
+class ABTest{
+    static {
+        System.out.println("Static block AB");
+    }
+    public static void main(String[] args) {
+        B b = new B();
+    }
+}
+
