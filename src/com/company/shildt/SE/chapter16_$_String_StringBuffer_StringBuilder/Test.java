8dc632639f842be035ea323649bb7f4bf4350bc0
Shvetsovd <shvetsovd@gmail.com>
2017-05-16 16:59:02 +0400

    16052017

--- src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test.java
+++ src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test.java
@@ -7,18 +7,18 @@ public class Test {
     public static void main(String[] args) {
         //0123456789...       20
         //This is a test string
-        String test = "This is a test string";
+        String s = "This is a test string";
         String result;
 
-        System.out.println(test.length()); //21
-
-        //substring(int beginIndex, int endIndex) - [beginIndex; endIndex)
-        result = test.substring(0,3); // Thi
-        result = test.substring(5,21); // is a test string
-
-        result = test.replace("is", "**"); // Th** ** a test string
-        result = test.replace(" ", "_"); // This_is_a_test_string
-
+        result = s.substring(5);
         System.out.println(result);
+
+        result = s.substring(0, 3);
+        System.out.println(result);
+
+        result = s.substring(5, 11);
+        System.out.println(result);
+
+
     }
 }
--- src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
+++ src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
@@ -1,23 +1,24 @@
 package company.shildt.SE.chapter16_$_String_StringBuffer_StringBuilder;
 
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.util.Arrays;
 
 public class Test2 {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        double d = -9E100;
-        long l = (long)d;
-        int i = (int)d;
-        short s = (short) d;
-        char c = (char) d;
-        byte b = (byte) d;
-
-        System.out.println(l);
-        System.out.println(i);
-        System.out.println(s);
-        System.out.println((int)c);
-        System.out.println(b);
+        String s = "This is a test string";
+        System.out.println(s.matches("\\d")); //false - цифра(один раз)
+        System.out.println(s.matches("\\D?")); //false - не цифра(ноль или один раз)
+        System.out.println(s.matches("\\D")); //false - не цифра(один раз)
+        System.out.println(s.matches("\\D*")); //true - не цифра (ноль или много раз)
+        System.out.println(s.matches("\\D+")); //true - не цифра (1 или много)
+        System.out.println(s.matches("[A-z\\s]+")); //true - любая буква от A до z или пробел (один или много раз)
+        System.out.println(s.matches("[\\w\\s]+")); //true - любая буква или пробел (один или много раз)
+        System.out.println(s.matches("[\\s\\w]+")); //true - тоже самое, что и предыдущее
+        System.out.println(s.matches("[^\\d э-я]+")); //true - не цифра и не э, ю, я (один или много раз)
 
 
     }
