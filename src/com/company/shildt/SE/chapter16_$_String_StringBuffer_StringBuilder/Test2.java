896dd18ac5423fc5b24491503ee495c990ac36f2
Shvetsovd <shvetsovd@gmail.com>
2017-05-17 19:34:43 +0400

    17052017

--- src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
+++ src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
@@ -9,17 +9,15 @@ import java.util.Arrays;
 
 public class Test2 {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String s = "This is a test string";
-        System.out.println(s.matches("\\d")); //false - цифра(один раз)
-        System.out.println(s.matches("\\D?")); //false - не цифра(ноль или один раз)
-        System.out.println(s.matches("\\D")); //false - не цифра(один раз)
-        System.out.println(s.matches("\\D*")); //true - не цифра (ноль или много раз)
-        System.out.println(s.matches("\\D+")); //true - не цифра (1 или много)
-        System.out.println(s.matches("[A-z\\s]+")); //true - любая буква от A до z или пробел (один или много раз)
-        System.out.println(s.matches("[\\w\\s]+")); //true - любая буква или пробел (один или много раз)
-        System.out.println(s.matches("[\\s\\w]+")); //true - тоже самое, что и предыдущее
-        System.out.println(s.matches("[^\\d э-я]+")); //true - не цифра и не э, ю, я (один или много раз)
+        StringBuilder sb = new StringBuilder("ABCDEF");
 
+        System.out.println(sb.length()); //6
+        System.out.println(sb.capacity()); //22
+
+        sb.trimToSize();
+
+        System.out.println(sb.length()); //6
+        System.out.println(sb.capacity()); //6
 
     }
 
