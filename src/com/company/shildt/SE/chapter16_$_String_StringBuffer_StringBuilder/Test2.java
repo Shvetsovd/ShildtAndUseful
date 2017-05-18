112be8b1b98be07bcfe5c86905062ef34d8c0f26
Shvetsovd <shvetsovd@gmail.com>
2017-05-18 19:32:05 +0400

    18052017

--- src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
+++ src/com/company/shildt/SE/chapter16_$_String_StringBuffer_StringBuilder/Test2.java
@@ -11,14 +11,16 @@ public class Test2 {
     public static void main(String[] args) throws UnsupportedEncodingException {
         StringBuilder sb = new StringBuilder("ABCDEF");
 
-        System.out.println(sb.length()); //6
+        System.out.println(sb.length());   //6
         System.out.println(sb.capacity()); //22
 
         sb.trimToSize();
 
-        System.out.println(sb.length()); //6
+        System.out.println(sb.length());   //6
         System.out.println(sb.capacity()); //6
+
 
     }
 
 }
+
