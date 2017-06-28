7304d6f16032893c2465bb25343571be83777013
Shvetsovd <shvetsovd@gmail.com>
2017-06-28 19:02:47 +0400

    28062017

---
+++ src/com/company/myUseful/video/VideoPlayer.java
@@ -0,0 +1,24 @@
+package company.myUseful.video;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class VideoPlayer extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        WebView webview = new WebView();
+        webview.getEngine().load(
+                "https://www.youtube.com/embed/EOQ0ZY8h1RE"
+        );
+
+        webview.setPrefSize(640, 390);
+
+        stage.setScene(new Scene(webview));
+        stage.setTitle("Video");
+        stage.show();
+    }
+}
