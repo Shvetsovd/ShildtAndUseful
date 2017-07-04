ded169ff58c33dc942823573606fb8f6f163f7b0
Shvetsovd <shvetsovd@gmail.com>
2017-07-05 01:21:36 +0300

    05072017

---
+++ src/com/company/myUseful/dateTime/DateTimeDemo.java
@@ -0,0 +1,46 @@
+package company.myUseful.dateTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ * Created by user on 04.07.2017.
+ */
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+
+        System.out.println();
+
+        System.out.println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
+        System.out.println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+        System.out.println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+        System.out.println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+
+        System.out.println();
+
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("HH':'mm':'ss', 'M")));
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("HH':'mm':'ss', 'MM")));
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("HH':'mm':'ss', 'MMM")));
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("HH':'mm':'ss', 'MMMM")));
+
+        System.out.println();
+
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd'-'MM'-'yyyy', 'HH':'mm");
+        LocalDateTime nyDateTime = LocalDateTime.parse(
+                    "31-12-2017, 23:59", f);
+
+        System.out.println(nyDateTime.format(f));
+
+    }
+}
---
+++ src/com/company/myUseful/swing/PaintDemo.java
@@ -0,0 +1,59 @@
+package company.myUseful.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by user on 05.07.2017.
+ */
+class PaintPanel extends JPanel{
+    Insets ins;
+    Random r;
+
+    public PaintPanel() {
+        this.setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+        r = new Random();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int x, y, x1, y1;
+
+        int height = this.getHeight();
+        int width = this.getWidth();
+
+        ins = this.getInsets();
+
+        for (int i = 0; i < 10; i++) {
+            x = r.nextInt(width - ins.left);
+            y = r.nextInt(height - ins.bottom);
+
+            x1 = r.nextInt(width - ins.left);
+            y1 = r.nextInt(height - ins.bottom);
+
+            g.drawLine(x, y, x1, y1);
+        }
+    }
+}
+public class PaintDemo {
+    JFrame jfrm;
+    PaintPanel pp;
+
+
+    public PaintDemo() {
+        jfrm = new JFrame("Paint Demo");
+        jfrm.setSize(200, 150);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        pp = new PaintPanel();
+        jfrm.add(pp);
+
+        jfrm.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()-> new PaintDemo());
+    }
+}
