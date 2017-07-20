3ee3a05e774367571822aa69d9fa4ff5b5438289
Shvetsovd <shvetsovd@gmail.com>
2017-07-20 15:08:04 +0300

    20072017

---
+++ src/com/company/myUseful/javabeans/IntrospectorDemo.java
@@ -0,0 +1,38 @@
+package company.myUseful.javabeans;
+
+import java.beans.*;
+
+/**
+ * Created by OPER on 11.07.2017.
+ */
+public class IntrospectorDemo {
+    public static void main(String[] args) {
+
+        try {
+            //Получить class
+            Class<?> c = Class.forName("company.myUseful.javabeans.Colors");
+
+            //получить beanInfo класса
+            BeanInfo beanInfo = Introspector.getBeanInfo(c);
+
+            //вывести в консоль все свойства и события по BeanInfo
+            //Свойства
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            System.out.println("Доступные свойства: ");
+            for (PropertyDescriptor pd : propertyDescriptors) {
+                System.out.println(pd.getName());
+            }
+
+            //События
+            System.out.println("\nСобытия: ");
+            EventSetDescriptor[] events = beanInfo.getEventSetDescriptors();
+            for (EventSetDescriptor e : events) {
+                System.out.println(e.getName());
+            }
+        } catch (ClassNotFoundException | IntrospectionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
