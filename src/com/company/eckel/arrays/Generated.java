67b70370c40c2dae7f892399bbad8ad8f6a93e18
Shvetsovd <shvetsovd@gmail.com>
2017-08-19 00:37:08 +0300

    19082017

---
+++ src/com/company/eckel/arrays/Generated.java
@@ -0,0 +1,21 @@
+package company.eckel.arrays;//: net/mindview/util/Generated.java
+
+import company.eckel.net.mindview.util.CollectionData;
+import company.eckel.net.mindview.util.Generator;
+
+import java.util.*;
+
+public class Generated {
+  // Fill an existing array:
+  public static <T> T[] array(T[] a, Generator<T> gen) {
+    return new CollectionData<T>(gen, a.length).toArray(a);
+  }
+  // Create a new array:
+  @SuppressWarnings("unchecked")
+  public static <T> T[] array(Class<T> type,
+      Generator<T> gen, int size) {
+    T[] a =
+      (T[])java.lang.reflect.Array.newInstance(type, size);
+    return new CollectionData<T>(gen, size).toArray(a);
+  }
+} ///:~
