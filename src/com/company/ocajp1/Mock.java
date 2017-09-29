182a09029118cebf92c79e1670fc736579ac2df3
Shvetsovd <shvetsovd@gmail.com>
2017-09-29 19:08:14 +0300

    29092017

---
+++ src/com/company/ocajp1/Mock.java
@@ -0,0 +1,12 @@
+package company.ocajp1;
+
+class Mock {
+
+    public static void main(String[] args) {
+        String[] strs = new String[3];
+        for(String s: strs){
+
+        }
+
+    }
+}
--- src/com/company/ocajp1/Test.java
+++
@@ -1,8 +0,0 @@
-package company.ocajp1;
-
-class Test {
-
-    public static void main(String[] args) {
-
-    }
-}
