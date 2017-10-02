68294c798a03765ac27c4d4a45675b00e7a92ad7
Shvetsovd <shvetsovd@gmail.com>
2017-10-02 19:16:50 +0300

    02102017

--- src/com/company/ocajp1/Mock.java
+++ src/com/company/ocajp1/Mock.java
@@ -1,10 +1,17 @@
 package company.ocajp1;
 
+import company.shildt.chapter3.Array;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Mock {
 
     public static void main(String[] args) {
-        int x = 0;
-        if (x > 0) System.out.println(" x > 0");
-        else System.out.println(" x <= 0");
+
     }
 }
