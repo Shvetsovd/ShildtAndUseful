d6d132f719c34166cdc24f66acd60a2271959a82
Shvetsovd <shvetsovd@gmail.com>
2017-10-03 18:30:48 +0300

    03102017

--- src/com/company/ocajp1/Mock.java
+++ src/com/company/ocajp1/Mock.java
@@ -1,17 +1,31 @@
 package company.ocajp1;
 
-import company.shildt.chapter3.Array;
+
+import company.eckel.net.mindview.util.RandomGenerator;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 class Mock {
 
     public static void main(String[] args) {
+        LocalDate date;
+        LocalTime time;
+        LocalDateTime dateTime;
+
+        Period period;
+        DateTimeFormatter formatter;
+
+        String x = new String("Hello World");
+        String y = "Hello World";
+        System.out.println(x == y); // false
+
 
     }
 }
