f784c228c632854b3168ab6e1b787ffbf0698f0e
Shvetsovd <shvetsovd@gmail.com>
2017-10-06 18:42:12 +0300

    06102017

--- src/com/company/ocajp1/Mock.java
+++ src/com/company/ocajp1/Mock.java
@@ -4,7 +4,6 @@ class Mock {
     public static void main(String[] args) {
         Mock mock = new Mock();
     }
-
 }
 
 class InitializationFuckBrainClass {
@@ -43,6 +42,47 @@ class InitializationFuckBrainClass {
 
     public static void main(String[] args) {
         System.out.println("Start...");
-    } //0 2 1 3 4 999 5 Start...
+    } //0 2 1 3 4 999 5 Strart...
 }
 
+class A{
+    static {
+        System.out.println("A1 ");
+    }
+    {
+        System.out.println("A2 ");
+    }
+    {
+        System.out.println("A3 ");
+    }
+    {
+        System.out.println("A4 ");
+    }
+    {
+        System.out.println("A5 ");
+    }
+
+    public A() {
+        super();
+        System.out.println("Aconstructor ");
+    }
+
+
+}
+class B extends A{
+    static {
+        System.out.println("B1 ");
+    }
+    {
+        System.out.println("B2 ");
+    }
+
+    public B() {
+        super();
+        System.out.println("Bconstructor ");
+    }
+
+    public static void main(String[] args) {
+         new B();
+    }
+}
