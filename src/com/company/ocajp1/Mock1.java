f126714a593804ff261003def67414e04b89ba28
Shvetsovd <shvetsovd@gmail.com>
2017-10-27 18:38:33 +0300

    27102017

--- src/com/company/ocajp1/Mock1.java
+++ src/com/company/ocajp1/Mock1.java
@@ -1,14 +1,21 @@
 package company.ocajp1;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 final public class Mock1 {
     public static void main(String[] args) {
-
     }
+
 }
+
+
+
 
 
 
