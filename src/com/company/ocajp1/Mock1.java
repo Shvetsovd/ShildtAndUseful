d267888717a45b71a049b3ab940be98144349d7c
Shvetsovd <shvetsovd@gmail.com>
2017-11-03 18:56:28 +0300

    03112017

--- src/com/company/ocajp1/Mock1.java
+++ src/com/company/ocajp1/Mock1.java
@@ -1,18 +1,35 @@
 package company.ocajp1;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 final public class Mock1 {
-    public static void main(String[] args) {
+    static public void main(String[] args) throws Exception {
+        Duration dur = Duration.ofHours(7);
+
+        System.out.println(dur);
+        /*DateTimeFormatter f = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        System.out.println(f.format(date1));*/
     }
 
 }
+
+interface Intf{
+    default void m(){}
+}
+class A{}
+class B extends A{
+    public void m(){}
+}
+class C extends B implements Intf{
+    void method(){
+        Intf.super.m();
+    }
+}
+class D extends C{}
+
 
 
 
